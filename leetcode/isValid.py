b04c90ad000757c46c3217b0a4afa13bd711059e
tamura <daiki1106tamu@gmail.com>
2024-12-18 20:45:53 +0900

    AC

---
+++ leetcode/isValid.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for i in s:
+            if i == "(" or i == "{" or i == "[":
+                stack.append(i)
+            else:
+                if i == ")" and len(stack) > 0 and stack[-1] == "(":
+                    stack.pop()
+                elif i == "}" and len(stack) > 0 and stack[-1] == "{":
+                    stack.pop()
+                elif i == "]" and len(stack) > 0 and stack[-1] == "[":
+                    stack.pop()
+                else:
+                    return False
+
+        return len(stack) == 0
