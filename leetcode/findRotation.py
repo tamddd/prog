5c4d8e8fdd09e04ba06cd743c4e710f74a3476b1
tamura <daiki1106tamu@gmail.com>
2024-12-18 19:30:45 +0900

    ちょっとコメント

--- leetcode/findRotation.py
+++ leetcode/findRotation.py
@@ -6,15 +6,19 @@ class Solution:
 
         for i in range(n):
             for j in range(n):
+                #90
                 if mat[i][j] != target[j][n-i-1]:
                     all_rotate[0] = False
 
+                #180
                 if mat[i][j] != target[n-i-1][n-j-1]:
                     all_rotate[1] = False
 
+                #270
                 if mat[i][j] != target[n-j-1][i]:
                     all_rotate[2] = False
 
+                #0
                 if mat[i][j] != target[i][j]:
                     all_rotate[3] = False
 
