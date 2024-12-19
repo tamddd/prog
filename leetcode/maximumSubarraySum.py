ab877504b1b167ea7ed2546e0983391f39a40370
tamura <daiki1106tamu@gmail.com>
2024-12-19 19:38:57 +0900

    全部訪れる必要はない

--- leetcode/maximumSubarraySum.py
+++ leetcode/maximumSubarraySum.py
@@ -1,6 +1,6 @@
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        numPositions = defaultdict(list)
+        numPositions = {}
         n = len(nums)
         prefix = [0] * (n+1)
         res = -inf+1
@@ -8,9 +8,14 @@ class Solution:
         for i, v in enumerate(nums):
             print(i, v-k)
             prefix[i+1] = prefix[i] + nums[i]
-            numPositions[v].append(i)
-            for pi in numPositions[v-k]:
+            if (v-k) in numPositions:
+                pi = numPositions[v-k]
                 res = max(res, prefix[i+1] - prefix[pi])
-            for pi in numPositions[v+k]:
+            if (v+k) in numPositions:
+                pi = numPositions[v+k]
                 res = max(res, prefix[i+1] - prefix[pi])
+            if v in numPositions and prefix[i+1] - prefix[numPositions[v]] < nums[i]:
+                numPositions[v] = i
+            if v not in numPositions:
+                numPositions[v] = i
         return res if res != -inf+1 else 0
