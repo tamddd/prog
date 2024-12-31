49396ab74c4bb3afb8abc860572173e1b3a960b9
tamura <daiki1106tamu@gmail.com>
2025-01-01 03:27:32 +0900

    OK

--- rust_backend/calc/src/main.rs
+++ rust_backend/calc/src/main.rs
@@ -1,5 +1,8 @@
 use std::io::stdin;
 fn main() {
+    let mut memory: f64 = 0.0;
+    let mut prev_result: f64 = 0.0;
+
     for line in stdin().lines() {
         let line = line.unwrap();
         if line.is_empty() {
@@ -7,9 +10,25 @@ fn main() {
         }
         let tokens: Vec<&str> = line.split(char::is_whitespace).collect();
 
-        let left: f64 = tokens[0].parse().unwrap();
-        let right: f64 = tokens[0].parse().unwrap();
+        if tokens[0] == "mem+" {
+            memory += prev_result;
+            println!("=>{}", memory);
+            continue;
+        } else if tokens[0] == "mem-" {
+            memory -= prev_result;
+            println!("=>{}", memory);
+        }
+        let left: f64 = if tokens[0] == "mem" {
+            memory
+        } else {
+            tokens[0].parse().unwrap()
+        };
 
+        let right: f64 = if tokens[2] == "mem" {
+            memory
+        } else {
+            tokens[2].parse().unwrap()
+        };
         let result = match tokens[1] {
             "+" => left + right,
             "-" => left - right,
