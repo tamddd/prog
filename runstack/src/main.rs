64df12fd89f5ac5745ed23c7cd6290a82cfe4635
tamura <daiki1106tamu@gmail.com>
2025-01-17 11:02:51 +0900

    OK

--- runstack/src/main.rs
+++ runstack/src/main.rs
@@ -1,8 +1,31 @@
 fn main() {
     let mut stack = vec![];
 
-    stack.push(32);
-    stack.push(123);
+    for line in std::io::stdin().lines() {
+        if let Ok(line) = line {
+            let words: Vec<_> = line.split(" ").collect();
+
+            for word in words {
+                if let Ok(parsed) = word.parse::<i32>() {
+                    stack.push(parsed);
+                } else {
+                    match word {
+                        "+" => add(&mut stack),
+                        _ => panic!("{word:?} could not be parsed"),
+                    }
+                }
+            }
+            println!("stack: {stack:?}");
+        }
+    }
+
+    add(&mut stack);
 
     println!("stack: {stack:?}")
+}
+
+fn add(stack: &mut Vec<i32>) {
+    let lhs = stack.pop().unwrap();
+    let rhs = stack.pop().unwrap();
+    stack.push(lhs + rhs);
 }
