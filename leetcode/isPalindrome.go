6d1670f2270744f7da8cdb9482eed28509474ef3
tamura <daiki1106tamu@gmail.com>
2025-01-20 08:53:53 +0900

    AC

--- leetcode/isPalindrome.go
+++ leetcode/isPalindrome.go
@@ -4,16 +4,35 @@ func main() {
 	return
 }
 
-func isPalindromeIterative(s string) bool {
-	left := 0
-	right := len(s) - 1
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
-	for left <= right {
-		if s[left] != s[right] {
+func isPalindrome(head *ListNode) bool {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	var prev *ListNode = nil
+	cur := slow
+	for cur != nil {
+		nex := cur.Next
+		cur.Next = prev
+		prev = cur
+		cur = nex
+	}
+
+	first_cur := head
+	second_cur := prev
+
+	for first_cur != nil && second_cur != nil {
+		if first_cur.Val != second_cur.Val {
 			return false
 		}
-		left++
-		right--
+		first_cur = first_cur.Next
+		second_cur = second_cur.Next
 	}
 	return true
 }
