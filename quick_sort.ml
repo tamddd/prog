f4f961e8e7f05093b3047a3b96e95380333e9f7b
tamura <daiki1106tamu@gmail.com>
2025-03-25 21:34:54 +0900

    test

--- quick_sort.ml
+++ quick_sort.ml
@@ -4,3 +4,10 @@ let rec quick_sort lst = match lst with
      let smaller_equal = List.filter (fun x -> x <= pivot) rest in
      let greater = List.filter (fun x -> pivot < x) rest in
      (quick_sort smaller_equal) @ [pivot] @ (quick_sort greater)
+
+
+let rec is_sorted lst pre = match lst with
+    [] -> true
+  | first :: rest ->
+     if first <= pre then is_sorted first rest
+     else false
