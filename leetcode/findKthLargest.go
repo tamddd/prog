c4881593341b55a0c0a2480c26ddc1f8ae213457
tamura <daiki1106tamu@gmail.com>
2025-10-27 18:03:11 +0900

    AC

---
+++ leetcode/findKthLargest.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import "container/heap"
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] > h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x any) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() any {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func findKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	*h = nums
+	heap.Init(h)
+	for i := 0; i < k-1; i++ {
+		heap.Pop(h)
+	}
+	return heap.Pop(h).(int)
+}
