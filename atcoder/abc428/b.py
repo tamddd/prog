61d20721391a4d44a9446f925a1c396351175714
tamura <daiki1106tamu@gmail.com>
2025-12-05 18:43:36 +0900

    AC

--- atcoder/abc428/b.py
+++ atcoder/abc428/b.py
@@ -1,6 +1,14 @@
 from collections import Counter
 n, k = map(int, input().split())
 s = input()
-ans = sorted(Counter([s[i:i+k] for i in range(n-k+1)]).items(), key=lambda x : x[1])[-1]
-print(ans[1])
-print(ans[0])
+c = Counter(s[i:i+k] for i in range(n-k+1))
+max_val = max(c.values())
+ans = []
+for i in c.items():
+  if i[1] == max_val:
+    ans.append(i[0])
+ans.sort()
+
+print(max_val)
+for i in ans:
+  print(i)
