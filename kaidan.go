82942e8b441b2b97a49beb1453142d679bf425f3
tamura <daiki1106tamu@gmail.com>
2025-12-16 22:00:41 +0900

    AC

--- kaidan.go
+++ kaidan.go
@@ -1,4 +1,4 @@
-package main
+/*package main
 
 import (
 	"fmt"
@@ -15,5 +15,29 @@ func main() {
 		dp[i] = dp[i-1] + dp[i-2]
 	}
 
+	fmt.Println(dp[n])
+}
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scanf("%d", &n)
+
+	dp := make([]int, n+1)
+	dp[0] = 1
+	for i := 1; i < n+1; i++ {
+		if 0 <= i-1 {
+			dp[i] += dp[i-1]
+		}
+		if 0 <= i-2 {
+			dp[i] += dp[i-2]
+		}
+	}
 	fmt.Println(dp[n])
 }
