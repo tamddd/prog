d709539bfeb67980a2d0f4ed984aadf5d41e952c
tamura <daiki1106tamu@gmail.com>
2025-12-16 17:57:37 +0900

    違う書き方を

--- histogram.py
+++ histogram.py
@@ -6,3 +6,12 @@ c = Counter(i//10 for i in list(map(int, input().split())))
 
 for i in range(10):
     print(c[i])
+
+"""
+from collections import Counter
+n = int(input())
+d = Counter(list(map(lambda x : int(x) // 10, input().split())))
+
+for i in range(0, 10):
+    print(d[i])
+"""
