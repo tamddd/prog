8745bc0aeee316491d3838fc6554877c03dffd44
tamura <daiki1106tamu@gmail.com>
2025-12-25 16:11:35 +0900

    AC

---
+++ ajMax.py
@@ -0,0 +1,11 @@
+n = int(input())
+a = list(map(int, input().split()))
+
+cnt = 0
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if a[i] <= a[j] and a[k] <= a[j]:
+                cnt += 1
+
+print(cnt)
